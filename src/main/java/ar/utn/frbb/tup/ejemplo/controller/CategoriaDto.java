97cab467ff22c79301ef056ed22759894a76f67c
luciano.salotto <lsalotto@gmail.com>
2024-06-27 14:21:32 -0300

    Add presentation layer Unit Testing

---
+++ src/main/java/ar/utn/frbb/tup/ejemplo/controller/CategoriaDto.java
@@ -0,0 +1,23 @@
+package ar.utn.frbb.tup.ejemplo.controller;
+
+public class CategoriaDto {
+
+    private String nombre;
+    private String descripcion;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
